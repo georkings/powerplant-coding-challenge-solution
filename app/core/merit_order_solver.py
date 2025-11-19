773b044510a4058234ff16368611bcfb2070d49c
DIUSFRAMI\jareyes <jareyes@diusframi.es>
2025-11-19 10:35:38 +0100

    Add file merit_order_solver.py to calculate production plan (to be implemented)

---
+++ app/core/merit_order_solver.py
@@ -0,0 +1,9 @@
+# powerplant-challenge/app/core/merit_order_solver.py
+
+from app.models.data_models import InputPayload, ProductionPlanItem
+
+def calculate_production_plan(payload: InputPayload) -> list[ProductionPlanItem]:
+    # TODO
+    final_plan = []
+
+    return final_plan
