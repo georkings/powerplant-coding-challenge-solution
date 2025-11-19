5c5af20f0bc32a59529368b9abe7ade7f4129347
DIUSFRAMI\jareyes <jareyes@diusframi.es>
2025-11-19 16:46:10 +0100

    Add logging configuration to add timestamp to logs

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # powerplant-challenge/app/main.py
 
+import logging
 from fastapi import FastAPI
 from app.routers import production_plan
+
+# Logging configuration
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 
 # FastAPI Application Setup
 app = FastAPI()
